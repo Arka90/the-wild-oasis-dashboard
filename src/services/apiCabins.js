e0d31901b594f3546c48f28405c4e9035aa04af8
Arka Sheikh <skarka90@gmail.com>
2023-08-03 02:50:03 +0530

    Added react query

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -5,8 +5,17 @@ export async function getCabins() {
 
   if (error) {
     console.log(error);
-    throw new Error("Cabin could not be loaded");
+    throw new Error("Cabins could not be loaded");
   }
 
   return data;
+}
+
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    console.log(error);
+    throw new Error("Cabin could not be deleted");
+  }
 }
